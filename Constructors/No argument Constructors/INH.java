06b89b4393fa53bf203371ee55ef72ad5d1ec708
sajithlakshan <57319180+sajithlakshan@users.noreply.github.com>
2022-02-23 23:28:45 +0530

    Update INH.java

--- Constructors/No argument Constructors/INH.java
+++ Constructors/No argument Constructors/INH.java
@@ -1,1 +1,15 @@
-sss
+class MyClass {
+    int num;
+    MyClass() {
+        num = 100;
+    }
+}
+
+
+public class INH {
+    public static void main(String args[]) {
+        MyClass t1 = new MyClass();
+        MyClass t2 = new MyClass();
+        System.out.println(t1.num + " " + t2.num);
+    }
+}
