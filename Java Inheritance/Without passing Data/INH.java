ec0c621ab96bda34d0f342e8110bbaad1b79d3a3
sajithlakshan <57319180+sajithlakshan@users.noreply.github.com>
2022-02-24 12:22:44 +0530

    Create INH.java

---
+++ Java Inheritance/Without passing Data/INH.java
@@ -0,0 +1,39 @@
+class Vehicle {
+     String brand;
+     String Gos ;
+     String Park = "OK";
+     private String breal = "DDR";
+    public void SOKAT_Sober() {
+        System.out.println("I have");
+    }
+    public  void Print_variable(){
+        System.out.println(brand+" "+Gos+" "+Park+" "+breal);
+    }
+}
+class car extends Vehicle {
+    String color = "Black";
+    String Engine_No = "55335466";
+    public void Remove_Tiar(){
+        System.out.println("I Can remove tiear");
+    }
+
+}
+public class INH {
+    public static void main(String[] args) {
+        Vehicle V_N1 = new Vehicle();
+        V_N1.SOKAT_Sober();
+
+        V_N1.brand="Ford";
+        V_N1.Gos = "ON Road";
+        V_N1.Park = "Jemini";
+//We canot access this : Becouse  -- private
+        //V_N1.breal ="DOmi";
+        V_N1.Print_variable();
+
+
+        car C1 =new car();
+        C1.color = "Jemini";
+        C1.Remove_Tiar();
+
+    }
+}
