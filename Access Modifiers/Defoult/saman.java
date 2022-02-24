bb43fe0bb86c55f0d7823f95c0b90acbca0096e3
sajithlakshan <57319180+sajithlakshan@users.noreply.github.com>
2022-02-24 17:27:00 +0530

    Create saman.java

---
+++ Access Modifiers/Defoult/saman.java
@@ -0,0 +1,12 @@
+package Pac2;
+
+import Pac1.nimal;
+
+public class saman {
+    public static void main(String args[]){
+        nimal DD = new  nimal();
+ //Canot use this statement
+//        DD.EE="prot";
+
+    }
+}
