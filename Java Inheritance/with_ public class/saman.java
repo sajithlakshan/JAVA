d58bd32b774f9671dfff45d2935a5f46fc22d6bd
sajithlakshan <57319180+sajithlakshan@users.noreply.github.com>
2022-02-24 18:41:13 +0530

    Create saman.java

---
+++ Java Inheritance/with_ public class/saman.java
@@ -0,0 +1,11 @@
+package Pac2;
+
+import Pac1.*;
+
+public class saman extends nimal{
+    public static void main(String args[]){
+        saman OBj = new saman();
+        OBj.EE = "iowii";
+
+    }
+}
