0df5d858b144d4a020842cd86a6bb5ad21fce733
sajithlakshan <57319180+sajithlakshan@users.noreply.github.com>
2022-02-24 08:19:51 +0530

    Create INH.java

---
+++ Overload/IN Two clases/INH.java
@@ -0,0 +1,18 @@
+class OVER {
+    static int plusMethod(int x, int y) {
+        return x + y;
+    }
+
+    static double plusMethod(int x, int y, int Z) {
+        return x + y + Z;
+    }
+
+}
+public class INH {
+    public static void main(String[] args) {
+    System.out.println(OVER.plusMethod(2,3));
+    System.out.println(OVER.plusMethod(2,3,100));
+
+    }
+}
+
