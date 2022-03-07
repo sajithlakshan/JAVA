af6384338f36510d2b2cc7600443e3915a2eb753
sajithlakshan <57319180+sajithlakshan@users.noreply.github.com>
2022-03-07 15:32:51 +0530

    Create main.cpp

---
+++ Multithreading in Java/EX_4_ RuntimeException/main.cpp
@@ -0,0 +1,35 @@
+
+class MultiThread extends Thread{
+
+    public int threadNumber;
+    public MultiThread(int threadNumber){
+        this.threadNumber = threadNumber;
+    }
+    @Override
+    public void run(){
+
+        for(int i =1; i <=5 ;i++)
+        {
+            System.out.println(i+ " Thread Number "+threadNumber);
+            if(threadNumber == 3){
+                throw new RuntimeException();
+            }
+            try{
+                Thread.sleep(1000);
+            }catch (InterruptedException e){
+
+            }
+        }
+    }
+}
+public class INH {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 3 ; i++)
+        {
+            MultiThread multiThread = new MultiThread(i);
+            multiThread.start();
+        }
+
+    }
+}
