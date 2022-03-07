4ce9c64a8fe65fbdb6145b941acf80a57df2f816
sajithlakshan <57319180+sajithlakshan@users.noreply.github.com>
2022-03-07 15:55:42 +0530

    Create main.cpp

---
+++ Multithreading in Java/Implements Runnable/work thread one After Another/main.cpp
@@ -0,0 +1,38 @@
+
+class MultiThread implements Runnable{
+
+    public int threadNumber;
+    public MultiThread(int threadNumber){
+        this.threadNumber = threadNumber;
+    }
+    @Override
+    public void run(){
+
+        for(int i =1; i <=5 ;i++)
+        {
+            System.out.println(i+ " Thread Number "+threadNumber);
+            try{
+                Thread.sleep(1000);
+            }catch (InterruptedException e){
+
+            }
+        }
+    }
+}
+public class INH {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 3 ; i++)
+        {
+            MultiThread multiThread = new MultiThread(i);
+            Thread myThread = new Thread(multiThread);
+            myThread.start();
+            try{
+                myThread.join();
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
