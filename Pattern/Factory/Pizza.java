5dba3ff9db514a59a01a02977197b88f99ce0393
sajithlakshan <57319180+sajithlakshan@users.noreply.github.com>
2022-06-17 14:22:35 +0530

    Update Pizza.java

--- Pattern/Factory/Pizza.java
+++ Pattern/Factory/Pizza.java
@@ -1,12 +1,7 @@
 package FactoryPattern;
 
-public class Client {
-
-	public static void main(String[] args) {
-		PizzaFactory ob = new PizzaFactory();
-		Pizza Pi = ob.createPizza("Veggie");
-		Pi.servePizza();
-
-	}
+public abstract class Pizza {
+	abstract void servePizza();
 
 }
+
