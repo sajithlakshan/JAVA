dd67aea71a2cd4b71bab9bc9201f9a8a16e6b0ea
sajithlakshan <57319180+sajithlakshan@users.noreply.github.com>
2022-08-22 06:33:44 +0530

    Add files via upload

---
+++ Call another class in method/INH.java
@@ -0,0 +1,29 @@
+class ROTI{
+	void Godamba() {
+		System.out.println("FFFFFF");
+	}
+}
+
+class SIMAN{
+     
+	ROTI SIMAN(){
+		ROTI kko = new ROTI();
+	
+		return kko;
+	}
+	void VIBONS() {
+		System.out.println("Nigaaaaaaaaaa");
+	}
+}
+
+
+
+class INH {
+  public static void main(String[] args) {
+	  SIMAN pp = new SIMAN();
+	  pp.SIMAN().Godamba();
+	  
+		  
+	  
+  }
+}
