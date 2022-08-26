54997ad88f23322da0900bc490def2d063fbce51
sajithlakshan <57319180+sajithlakshan@users.noreply.github.com>
2022-08-26 12:59:48 +0530

    Add files via upload

---
+++ Anonimous Function/Pass Data/Printable.java
@@ -0,0 +1,21 @@
+package Ex1;
+interface Printable {
+	void Sajith(String suffix);
+
+
+
+}
+
+
+class Cat implements Printable{
+
+
+	@Override
+	public void Sajith(String suffix) {
+		// TODO Auto-generated method stub
+	 
+		System.out.println("Miyau");
+		
+	}
+	
+}
---
+++ Anonimous Function/Pass Data/main.java
@@ -0,0 +1,20 @@
+package Ex1;
+
+import java.util.Stack;
+
+public class main {
+
+	public static void main(String[] args) {
+              
+		Printable klk = (s)-> System.out.println("12" +s);
+		printTHing(klk);
+		
+
+	}
+	
+	
+	static void printTHing(Printable thing) {
+		thing.Sajith("!");
+	}
+
+} 
